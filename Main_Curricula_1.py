95315b34d60edab2d512957463d40054d094b370
Grayson Schaer <gschaer2@illinois.edu>
2020-10-22 22:33:44 -0500

    Reduced learning rate. increased gamma

--- Main_Curricula_1.py
+++ Main_Curricula_1.py
@@ -97,8 +97,8 @@ if __name__ == '__main__':
     num_hidden_layers = 2
     num_neurons_in_layer = 128
     clone_interval = 10000
-    alpha = 0.00030
-    gamma = 0.99
+    alpha = 0.00025
+    gamma = 0.995
     epsilon_start = 1.00
     epsilon_end = 0.10
     epsilon_depreciation_factor = math.pow(epsilon_end,(3.0 / (n_episodes*n_steps - start_data_set_size)))
